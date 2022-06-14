acc7b9863b88d65bc6139a50f358e69a19a4e3be
Nidhi-hub-pixel <76686078+Nidhi-hub-pixel@users.noreply.github.com>
2022-06-13 23:03:21 -0700

    Add files via upload

---
+++ problem 4.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+
+path = ''
+
+
+l_files = os.listdir(path)
+
+
+for file in l_files:
+
+
+	file_path = f'{path}\\{file}'
+
+	
+	if os.path.isfile(file_path):
+		try:
+			
+			os.startfile(file_path, 'print')
+			print(f'Printing {file}')
+
+			
+			time.sleep(5)
+		except:
+			
+			print(f'ALERT: {file} could not be printed! Please check\
+			the associated softwares, or the file type.')
+	else:
+		print(f'ALERT: {file} is not a file, so can not be printed!')
+		
+print('Task finished!')
